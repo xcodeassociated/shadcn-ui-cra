3f4b319b665a110558608909ad44134c249a68e0
xcodeassociated <janusz.majchrzak@protonmail.com>
2024-06-24 20:02:32 +0200

    fix header z index

--- src/App.tsx
+++ src/App.tsx
@@ -545,7 +545,7 @@ function App() {
 
   return (
     <div className="flex min-h-screen w-full flex-col">
-      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-10">
         <nav
           className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Label className="flex items-center gap-2 text-lg font-semibold md:text-base cursor-pointer"
