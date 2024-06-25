df75985445623519130d6a12f0e537177681c569
xcodeassociated <janusz.majchrzak@protonmail.com>
2024-06-25 22:34:48 +0200

    add better table pagination

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,9 @@ import { z } from 'zod'
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { OnChangeFn } from '@tanstack/table-core/src/types'
 
+import { ChevronLeftIcon, ChevronRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from '@radix-ui/react-icons'
+import { Table as TTable } from '@tanstack/react-table'
+
 export interface Role {
   readonly _id: string | undefined
   name: string
@@ -488,6 +491,91 @@ const columns: ColumnDef<User>[] = [
   },
 ]
 
+interface DataTablePaginationProps<TData> {
+  table: TTable<TData>
+  withSelected?: boolean
+}
+
+export function DataTablePagination<TData>({ table, withSelected }: DataTablePaginationProps<TData>) {
+  return (
+    <div className="flex items-center justify-between px-2">
+      <div className="flex-1 text-sm text-muted-foreground">
+        {withSelected ? (
+          <>
+            {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s)
+            selected.
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className="flex items-center space-x-6 lg:space-x-8">
+        <div className="flex items-center space-x-2">
+          <p className="text-sm font-medium">Rows per page</p>
+          <Select
+            value={`${table.getState().pagination.pageSize}`}
+            onValueChange={(value) => {
+              table.setPageSize(Number(value))
+            }}
+          >
+            <SelectTrigger className="h-8 w-[70px]">
+              <SelectValue placeholder={table.getState().pagination.pageSize} />
+            </SelectTrigger>
+            <SelectContent side="top">
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <SelectItem key={pageSize} value={`${pageSize}`}>
+                  {pageSize}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+          Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+        </div>
+        <div className="flex items-center space-x-2">
+          <Button
+            variant="outline"
+            className="hidden h-8 w-8 p-0 lg:flex"
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Go to first page</span>
+            <DoubleArrowLeftIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Go to previous page</span>
+            <ChevronLeftIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Go to next page</span>
+            <ChevronRightIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="hidden h-8 w-8 p-0 lg:flex"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Go to last page</span>
+            <DoubleArrowRightIcon className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
@@ -553,23 +641,8 @@ export const DataTable = <TData, TValue>({
             </TableBody>
           </Table>
         </div>
-        <div className="my-4 flex items-center">
-          <div className="ml-auto text-sm text-muted-foreground">
-            {pagination.pageIndex + 1} of {table.getPageCount()}
-          </div>
-          <div className="ml-2 space-x-2">
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              Previous
-            </Button>
-            <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
-              Next
-            </Button>
-          </div>
+        <div className="py-2">
+          <DataTablePagination table={table} withSelected={false} />
         </div>
       </div>
     </div>
@@ -596,7 +669,10 @@ const Users = () => {
 
   useEffect(() => {
     getUsers(new Page(pagination.pageIndex, pagination.pageSize, 'id', 'ASC'))
-      .then((data) => setUsers(data))
+      .then((data) => {
+        console.log(`Pagination changed: ${JSON.stringify(pagination)}`)
+        setUsers(data)
+      })
       .catch((e) => console.error(e))
   }, [pagination])
 
